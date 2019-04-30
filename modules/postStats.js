b42c1deea82f5ad0ffa18f68e7240072e22520db
AscendantDev <mrlazerbird.official@hotmail.com>
2019-04-30 11:52:56 -0400

    Added the statistics poster.

---
+++ modules/postStats.js
@@ -0,0 +1,9 @@
+module.exports = async (client) => {
+    const Boats = require("boats.js");
+    const api = new Boats(client.config.boatsToken);
+
+    api.postStats(client.guilds.size, client.user.id);
+    setInterval(() => {
+        api.postStats(client.guilds.size, client.user.id);
+    }, 1000);
+};
