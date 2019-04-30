903c153be2677835af131169bc0cf5385a3804b6
AxorianDev <mrlazerbird.official@hotmail.com>
2019-04-30 19:58:01 -0400

    Added the member count command.

---
+++ commands/membercount.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+    function checkBots(guild) {
+        let botCount = 0;
+        guild.members.forEach(member => {
+            if (member.user.bot) botCount++;
+        });
+
+        return botCount;
+    }
+
+    function checkMembers(guild) {
+        let memberCount = 0;
+        guild.members.forEach(member => {
+            if (!member.user.bot) memberCount++;
+        });
+
+        return memberCount;
+    }
+
+    const embed = new Discord.RichEmbed()
+    embed.setTitle("Server Member Count");
+    embed.addField("Total Members", `${message.guild.memberCount}`);
+    embed.addField("Humans", `${checkMembers(message.guild)}`);
+    embed.addField("Bots", `${checkBots(message.guild)}`);
+    embed.setColor("BLACK");
+    message.channel.send({ embed: embed });
+};
