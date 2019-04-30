054170696be763ba135dadedcdd77db693de884f
April Freeman <38700239+AxorianDev@users.noreply.github.com>
2019-04-30 15:47:57 -0400

    Added the second guild event.

---
+++ events/guildDelete.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+module.exports = async (client, guild) => {
+    const embed = new Discord.RichEmbed()
+    embed.setTitle("Guild Delete");
+    embed.addField("Response", "I have been removed from a guild.");
+    embed.addField("Guild Name", `${guild.name} (${guild.id})`);
+    embed.setColor("RED");
+    client.channels.get("571890862074626078").send({ embed: embed });
+};
