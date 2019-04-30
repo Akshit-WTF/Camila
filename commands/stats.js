edc1f76b083f9cdcf1e1ce2f87736c99e8e2ccd3
April Freeman <38700239+AxorianDev@users.noreply.github.com>
2019-04-30 16:06:12 -0400

    Added the stats command.

---
+++ commands/stats.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const { version } = require("discord.js");
+
+module.exports = async (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+    embed.addField("Memory Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`);
+    embed.addField("Bot Version", `${client.instance.version}`);
+    embed.addField("Bot API", `${client.api.version}`);
+    embed.addField("Bot Library", `${version}`);
+    embed.addField("Guilds", `${client.guilds.size.toLocaleString()}`);
+    embed.addField("Channels", `${client.channels.size.toLocaleString()}`);
+    embed.addField("Users", `${client.users.size.toLocaleString()}`);
+    embed.setColor("BLACK");
+    message.channel.send({ embed: embed });
+};
