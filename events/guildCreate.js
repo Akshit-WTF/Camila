8b1227e94e72a8176c03dec12781753fe87170e8
April Freeman <38700239+AxorianDev@users.noreply.github.com>
2019-04-30 15:47:35 -0400

    Added the first guild event.

---
+++ events/guildCreate.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+module.exports = async (client, guild) => {
+    const embed = new Discord.RichEmbed()
+    embed.setTitle("Guild Create");
+    embed.addField("Response", "I have been added to a new guild.");
+    embed.addField("Guild Name", `${guild.name} (${guild.id})`);
+    embed.setColor("GREEN");
+    client.channels.get("571890862074626078").send({ embed: embed });
+};
