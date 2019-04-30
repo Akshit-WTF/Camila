738d90e26eb9d8a1e2f6141ee59a154b3141eb00
April Freeman <38700239+AxorianDev@users.noreply.github.com>
2019-04-30 16:21:25 -0400

    Added the invite command.

---
+++ commands/invite.js
@@ -0,0 +1,3 @@
+exports.run = async (client, message, args) => {
+    message.channel.send("If you would like to invite me to your guild, please visit this Discord Boats link: <https://discord.boats/bot/571813744271949844>");
+};
