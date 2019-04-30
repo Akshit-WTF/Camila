3f8697a1404e17305179b38c712640657cf26d3d
AxorianDev <mrlazerbird.official@hotmail.com>
2019-04-30 17:57:54 -0400

    Added server greetings module.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,14 @@
-module.exports = async (client) => {
+module.exports = async (client, member) => {
     client.user.setPresence(`${client.users.size} Users`, {
         type: "WATCHING",
         status: "dnd"
     });
+
+    const role = member.guild.roles.find(r => r.name === "Supporters");
+    if (!role) return undefined;
+    member.addRole(role).catch(() => null);
+
+    const channel = member.guild.channels.find(ch => ch.name === "meet-and-greet");
+    if (!channel) return undefined;
+    channel.send(`**${member.user.username}** has just joined.`).catch(() => null);
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,10 @@
-module.exports = async (client) => {
+module.exports = async (client, member) => {
     client.user.setPresence(`${client.users.size} Users`, {
         type: "WATCHING",
         status: "dnd"
     });
+
+    const channel = member.guild.channels.find(ch => ch.name === "meet-and-greet");
+    if (!channel) return undefined;
+    channel.send(`**${member.user.username}** has just left.`).catch(() => null);
 };
