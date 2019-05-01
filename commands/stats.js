5b4de89bbe78d6e8969d8822816f6a5702d01259
AxorianDev <mrlazerbird.official@hotmail.com>
2019-05-01 19:45:45 -0400

    This might be a possible fix?

--- commands/stats.js
+++ commands/stats.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
     const embed = new Discord.RichEmbed()
     embed.addField("Memory Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`);
     embed.addField("Bot Version", `${client.instance.version}`);
-    embed.addField("Bot API", `${client.api.settings.version}`);
+    embed.addField("Bot API", `${client.api.version}`);
     embed.addField("Bot Library", `${version}`);
     embed.addField("Guilds", `${client.guilds.size.toLocaleString()}`);
     embed.addField("Channels", `${client.channels.size.toLocaleString()}`);
