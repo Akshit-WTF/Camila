71952acfb4adae5ce4f10fc836b56314f5ea28e6
AxorianDev <mrlazerbird.official@hotmail.com>
2019-04-30 20:21:47 -0400

    Added the embed command.

---
+++ commands/embed.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+    const dataEmbed = args.join(" ");
+    if (!dataEmbed) return message.channel.send("Please supply text in which you'd like me to embed.");
+
+    const embed = new Discord.RichEmbed()
+    embed.setDescription(`${dataEmbed}`);
+    embed.setColor("RANDOM");
+    message.channel.send({ embed: embed });
+};
