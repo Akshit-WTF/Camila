1af69fc675ce33aa2beb010b1e4fdf7e9d8ae621
AxorianDev <mrlazerbird.official@hotmail.com>
2019-05-01 20:22:11 -0400

    Why did I try to init a new client?

--- commands/donate.js
+++ commands/donate.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-    const embed = new Discord.Client()
+    const embed = new Discord.RichEmbed()
     embed.setTitle("Donations");
     embed.setDescription("We appreciate all donations! Donations go towards server costs, domain costs, and other expenses. If you'd like to be a generous soul and donate, we'll give you some sweet perks such as a sweet role in our Discord and donator-only commands. To get the donation link, please direct message Axorian#5626");
     embed.setColor("BLACK");
