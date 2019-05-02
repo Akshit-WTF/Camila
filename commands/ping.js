63425982071783664114c52666425e22cc992f35
AxorianDev <mrlazerbird.official@hotmail.com>
2019-05-01 20:16:34 -0400

    Fixed the ping command.

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
+    const msg = await message.channel.send("Fetching the bot latency...");
+
     const embed = new Discord.RichEmbed()
-    embed.addField("Message Latency", `${Date.now() - message.createdTimestamp} milliseconds`);
+    embed.addField("Message Latency", `${msg.createdTimestamp - message.createdTimestamp} milliseconds`);
     embed.addField("API Latency", `${Math.round(client.ping)} milliseconds`);
     embed.addField("Shard Latency", `${Math.round(client.ping)} milliseconds`);
     embed.setColor("BLACK");
