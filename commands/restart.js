43a5679f9712ae546b28490eb91918beced803a9
AxorianDev <mrlazerbird.official@hotmail.com>
2019-05-02 19:31:51 -0400

    Fixed the permissions.

--- commands/restart.js
+++ commands/restart.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args) => {
-    if (message.author.id !== client.config.owner) {
+    if (message.author.id !== client.config.owner) return message.channel.send("You're missing the permissions to execute this command.");
+
+    message.channel.send("The process is currently restarting, this may take up to 5 minutes to complete.").then(() => {
         require("child_process").exec("pm2 restart 0");
-    } else {
-        return message.channel.send("You're missing the permissions to execute this command.");
-    }
+    });
 };
