914cebffd5bdb88dd5df78fbf9c6228cc417e8e0
AxorianDev <mrlazerbird.official@hotmail.com>
2019-05-01 20:29:52 -0400

    Fixed the guild create event.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,6 +6,7 @@ module.exports = async (client, guild) => {
     embed.addField("Response", "I have been added to a new guild.");
     embed.addField("Guild Name", `${guild.name} (${guild.id})`);
     embed.setColor("GREEN");
+    client.channels.get("571890862074626078").send({ embed: embed });
     
     client.user.setPresence(`${client.users.size} Users`, {
         type: "WATCHING",
