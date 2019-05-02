18539c8ab1a8cc697ae3920686faa75e48ad43cd
AxorianDev <mrlazerbird.official@hotmail.com>
2019-05-02 19:40:34 -0400

    Updated the restart command.

--- commands/restart.js
+++ commands/restart.js
@@ -2,6 +2,6 @@ exports.run = async (client, message, args) => {
     if (message.author.id !== client.config.owner) return message.channel.send("You're missing the permissions to execute this command.");
 
     message.channel.send("The process is currently restarting, this may take up to 5 minutes to complete.").then(() => {
-        require("child_process").exec("pm2 restart 0");
+        require("child_process").exec("git pull && pm2 restart 0");
     });
 };
