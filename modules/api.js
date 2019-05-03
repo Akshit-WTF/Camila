43c59d2e0d0390afba5d56b1e08f2247f78b28ce
April Freeman <38700239+AxorianDev@users.noreply.github.com>
2019-05-02 20:25:39 -0400

    Added bot owner statistic.

--- modules/api.js
+++ modules/api.js
@@ -2,7 +2,8 @@ const api = {
     os: "Camila",
     device: "Camila",
     version: "1.0.0",
-    birthday: "4/27/19"
+    birthday: "4/27/19",
+    owner: "Axorian#5626"
 };
 
 module.exports = api;
