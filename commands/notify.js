831f18e9f9e474a3e5d41082df0e02e1361182c9
April Freeman <38700239+AxorianDev@users.noreply.github.com>
2019-05-03 20:34:05 -0400

    Create notify.js

---
+++ commands/notify.js
@@ -0,0 +1,9 @@
+
+exports.run = async (client, message, args) => {
+    if (message.author.id !== client.config.owner) return message.channel.send("You're missing the permissions to execute this command.");
+
+    const dataMessage = args.join(" ");
+    if (!dataMessage) return undefined;
+    
+    client.users.get(args[1]).send(dataMessage);
+};
