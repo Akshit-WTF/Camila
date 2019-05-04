bb7cf51d2109bb941030a9e16036dfbaa482327b
April Freeman <38700239+AxorianDev@users.noreply.github.com>
2019-05-03 21:31:57 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = async (client) => {
     console.log(`Logged in as ${client.user.username}#${client.user.discriminator} and on ${client.guilds.size} guilds.`);
     client.user.setStatus("dnd");
-    client.user.setActivity(`${client.users.size} Users`, {
+    client.user.setActivity(`${client.users.size.toLocaleString()} Users`, {
         type: "WATCHING"
     });
 };
