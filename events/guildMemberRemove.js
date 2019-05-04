2e8888ed96a3b4e66c8d924af1a855da32855129
April Freeman <38700239+AxorianDev@users.noreply.github.com>
2019-05-03 21:32:51 -0400

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,9 +1,9 @@
 module.exports = async (client, member) => {
     client.user.setStatus("dnd");
-    client.user.setActivity(`${client.users.size} Users`, {
+    client.user.setActivity(`${client.users.size.toLocaleString()} Users`, {
         type: "WATCHING"
     });
-
+    
     const channel = member.guild.channels.find(ch => ch.name === "meet-and-greet");
     if (!channel) return undefined;
     channel.send(`**${member.user.username}** has just left.`).catch(() => null);
